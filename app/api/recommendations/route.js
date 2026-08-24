34cafecef5b720959ce38253f07622fefddb21a7
Alexey Kudryashov <akd@krononsoft.com>
2026-08-24 13:24:46 +0300

    Add more logs

--- app/api/recommendations/route.js
+++ app/api/recommendations/route.js
@@ -2,8 +2,11 @@ import { QdrantClient } from "@qdrant/qdrant-js";
 import { NextResponse } from "next/server";
 
 const client = new QdrantClient({
-  url: process.env.QDRANT_URL,
-  apiKey: process.env.QDRANT_API_KEY,
+  // url: process.env.QDRANT_URL,
+  // apiKey: process.env.QDRANT_API_KEY,
+  url: "https://c1c00031-838d-4cf2-aad7-55511bab358c.eu-west-2-0.aws.cloud.qdrant.io",
+  apiKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIiwic3ViamVjdCI6ImFwaS1rZXk6ZmIxMWZkNmMtNTlkNy00NDAzLTgxMGEtYmI3ZjBlNDQyNTljIn0.sv7y1Sr8T29xnBs-3i2kkdc_Q5Vk2yHixsh1uUBn33s",
 });
 
 const COLLECTION_NAME = "MasterpieceRecommendations";
@@ -24,7 +27,7 @@ export async function POST(request) {
     });
 
     if (!points || points.length === 0 || !points[0].vector) {
-      return NextResponse.json({ recommendations: getFallbackItems(history, currentLevelId) });
+      return NextResponse.json({ recommendations: getFallbackItems(history, currentLevelId, "noImages") });
     }
 
     const currentVector = points[0].vector;
@@ -52,7 +55,7 @@ export async function POST(request) {
     }));
 
     if (candidateItems.length < 3) {
-      return NextResponse.json({ recommendations: getFallbackItems(history, currentLevelId) });
+      return NextResponse.json({ recommendations: getFallbackItems(history, currentLevelId, "itsOk") });
     }
 
     // 3. Перемешиваем топ-10 и выбираем 3 элемента
@@ -62,17 +65,18 @@ export async function POST(request) {
     return NextResponse.json({ recommendations });
   } catch (error) {
     console.error("Qdrant Query Error:", error);
-    return NextResponse.json({ recommendations: getFallbackItems([], 0) });
+    return NextResponse.json({ recommendations: getFallbackItems([], 0, error) });
   }
 }
 
-function getFallbackItems(history, currentId) {
+function getFallbackItems(history, currentId, error) {
   const exclude = new Set([...history, currentId]);
   const result = [];
   while (result.length < 3) {
     const randomId = Math.floor(Math.random() * 1000) + 1;
     if (!exclude.has(randomId) && !result.some((item) => item.id === randomId)) {
       result.push({
+        error: error,
         pathId: `this_is_random_image/${randomId}`,
       });
     }
